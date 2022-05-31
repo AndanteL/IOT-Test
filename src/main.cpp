8b281b865bc56863c296bfe44c43e267d87c8046
Andante <191380469@qq.com>
2022-05-31 20:33:05 +0800

    Default Changelist

--- src/main.cpp
+++ src/main.cpp
@@ -257,22 +257,24 @@ bool AutoConfig()
 {
     WiFi.begin();
     int wstatus = WiFi.status();
-    if (wstatus == WL_CONNECTED)
-    {
-        Serial.println("WIFI SmartConfig Success");
-        Serial.printf("SSID:%s", WiFi.SSID().c_str());
-        Serial.printf(", PSW:%s\r\n", WiFi.psk().c_str());
-        Serial.print("LocalIP:");
-        Serial.print(WiFi.localIP());
-        Serial.print(" ,GateIP:");
-        Serial.println(WiFi.gatewayIP());
-        return true;
-    }
-    else
-    {
-        Serial.print("WIFI AutoConfig Faild!");
-        Serial.println(wstatus);
-        delay(1000);
+    for(int i = 0;i<20;i++){
+        if (wstatus == WL_CONNECTED)
+        {
+            Serial.println("WIFI SmartConfig Success");
+            Serial.printf("SSID:%s", WiFi.SSID().c_str());
+            Serial.printf(", PSW:%s\r\n", WiFi.psk().c_str());
+            Serial.print("LocalIP:");
+            Serial.print(WiFi.localIP());
+            Serial.print(" ,GateIP:");
+            Serial.println(WiFi.gatewayIP());
+            return true;
+        }
+        else
+        {
+            Serial.print("WIFI AutoConfig Faild!");
+            Serial.println(wstatus);
+            delay(1000);
+        }
     }
     return false;
 }
